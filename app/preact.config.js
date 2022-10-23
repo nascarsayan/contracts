ba276c4507beb06ac4f38e578477664e7aefdc6d
Sayan Naskar <nascarsayan@gmail.com>
2022-10-23 19:27:49 +0530

    disable purge css

--- app/preact.config.js
+++ app/preact.config.js
@@ -1,11 +1,11 @@
 module.exports = (config, env, helpers, params = defaultParams) => {
-  const purgecss = require("@fullhuman/postcss-purgecss")({
-    // Specify the paths to all of the template files in your project
-    content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  // const purgecss = require("@fullhuman/postcss-purgecss")({
+  //   // Specify the paths to all of the template files in your project
+  //   content: ["./src/**/*.{js,jsx,ts,tsx}"],
 
-    // Include any special characters you're using in this regular expression
-    defaultExtractor: (content) => content.match(params.regex) || [],
-  });
+  //   // Include any special characters you're using in this regular expression
+  //   defaultExtractor: (content) => content.match(params.regex) || [],
+  // });
 
   const postCssLoaders = helpers.getLoadersByName(config, "postcss-loader");
   postCssLoaders.forEach(({ loader }) => {
@@ -22,9 +22,9 @@ module.exports = (config, env, helpers, params = defaultParams) => {
     plugins.unshift(require("tailwindcss"));
 
     // Add PurgeCSS only in production.
-    if (env.production) {
-      plugins.push(purgecss);
-    }
+    // if (env.production) {
+    //   plugins.push(purgecss);
+    // }
   });
   return config;
 };
